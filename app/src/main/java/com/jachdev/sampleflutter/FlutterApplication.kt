9d8c9251f98ab4d596e1a42254bfb5d9878a0be6
Charitha Ratnayake <charitha.r@eyepax.com>
2021-05-24 00:30:57 +0530

    Added flutter channel and send response to flutter module

---
+++ app/src/main/java/com/jachdev/sampleflutter/FlutterApplication.kt
@@ -0,0 +1,31 @@
+package com.jachdev.sampleflutter
+
+import android.app.Application
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+/**
+ * Created by Charitha Ratnayake(charitha.r@eyepax.com) on 5/23/2021.
+ */
+class FlutterApplication: Application() {
+
+    lateinit var flutterEngine : FlutterEngine
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Instantiate a FlutterEngine.
+        flutterEngine = FlutterEngine(this)
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+            .getInstance()
+            .put("my_engine_id", flutterEngine)
+    }
+}
--- app/src/main/java/com/jachdev/sampleflutter/MainActivity.kt
+++ app/src/main/java/com/jachdev/sampleflutter/MainActivity.kt
@@ -2,10 +2,57 @@ package com.jachdev.sampleflutter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import kotlin.math.log
+import kotlin.math.log10
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "com.jachdev.flutter_module.host/login"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener(
+                View.OnClickListener {
+                    startActivity(
+                            FlutterActivity
+                                    .withCachedEngine("my_engine_id")
+                                    .build(this)
+                    )
+                }
+        )
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "login") {
+                val login = login()
+
+                if (0 == login) {
+                    result.success(login)
+                } else {
+                    result.error("UNAVAILABLE", "Login error.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun login(): Int {
+
+        Toast.makeText(context, "Call login api from native app.", Toast.LENGTH_SHORT).show()
+
+        return 0;
     }
 }
